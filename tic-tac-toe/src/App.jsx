174e06f806a69e6c17d375f3276d40f8805e4388
Rohit Sai Satya Rampalli <rohitsaisatya@gmail.com>
2024-11-25 14:01:37 +0530

    added feature which shows  'You are currently  at move #…' instead of a button.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-function Grid( {value, onGridClick} ) {
-  return  <button className="grid" onClick={onGridClick}>{value}</button>
+function Grid({ value, onGridClick }) {
+  return <button className="grid" onClick={onGridClick}>{value}</button>
 }
 
-function Board({isXNext, grid, onPlay}) {
+function Board({ isXNext, grid, onPlay }) {
   function handleClick(i) {
 
     if (grid[i] !== null || calculateWinner(grid)) {
@@ -32,26 +32,26 @@ function Board({isXNext, grid, onPlay}) {
   if (winner) {
     status = "Winner: " + winner;
   } else {
-    status = "Next player: " + (isXNext ? "X" : "O")
+    status = "Current player: " + (isXNext ? "X" : "O")
   }
 
   return (
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Grid value={grid[0]} onGridClick={() => handleClick(0)}/>
-        <Grid value={grid[1]} onGridClick={() => handleClick(1)}/>
-        <Grid value={grid[2]} onGridClick={() => handleClick(2)}/>
+        <Grid value={grid[0]} onGridClick={() => handleClick(0)} />
+        <Grid value={grid[1]} onGridClick={() => handleClick(1)} />
+        <Grid value={grid[2]} onGridClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Grid value={grid[3]} onGridClick={() => handleClick(3)}/>
-        <Grid value={grid[4]} onGridClick={() => handleClick(4)}/>
-        <Grid value={grid[5]} onGridClick={() => handleClick(5)}/>
+        <Grid value={grid[3]} onGridClick={() => handleClick(3)} />
+        <Grid value={grid[4]} onGridClick={() => handleClick(4)} />
+        <Grid value={grid[5]} onGridClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Grid value={grid[6]} onGridClick={() => handleClick(6)}/>
-        <Grid value={grid[7]} onGridClick={() => handleClick(7)}/>
-        <Grid value={grid[8]} onGridClick={() => handleClick(8)}/>
+        <Grid value={grid[6]} onGridClick={() => handleClick(6)} />
+        <Grid value={grid[7]} onGridClick={() => handleClick(7)} />
+        <Grid value={grid[8]} onGridClick={() => handleClick(8)} />
       </div>
     </>
   );
@@ -64,7 +64,7 @@ export default function Game() {
 
   const currentGrid = history[currentIndex]; // Board state of the last move
 
-  function handlePlay(copyOfGrid) {    
+  function handlePlay(copyOfGrid) {
     const nextHistory = [...history.slice(0, currentIndex + 1), copyOfGrid];
     setHistory(nextHistory);
     setCurrentIndex(nextHistory.length - 1);
@@ -88,10 +88,16 @@ export default function Game() {
     )
   })
 
+  const currentMoveDescription = currentIndex > 0 ? `You are at move #${currentIndex + 1}`
+    : "You are at the game's initial state"
+
 
   return (
     <div className="game">
-      <div className="game-board"> <Board isXNext={isXNext} grid={currentGrid} onPlay={handlePlay}/></div>
+      <div className="game-board"> 
+      <Board isXNext={isXNext} grid={currentGrid} onPlay={handlePlay} />
+      <div className="current-move">{currentMoveDescription}</div>
+      </div>
       <div className="game-info">{moves}</div>
     </div>
   )
@@ -112,7 +118,7 @@ function calculateWinner(grid) {
   ];
 
   for (let line of winningLines) {
-    const [a,b,c] = line; //  Destructure to get the three indexes in this line
+    const [a, b, c] = line; //  Destructure to get the three indexes in this line
 
     const isNotEmpty = grid[a];
     const isEqualAB = grid[a] === grid[b];
